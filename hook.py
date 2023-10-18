3b3eac1a4367558c115f007118abc358b8f05f3f
MahmoudElSayed100 <mahmoudelsayed121810@gmail.com>
2023-10-18 13:11:12 +0300

    done but has bugs

--- hook.py
+++ hook.py
@@ -2,6 +2,7 @@ from database_handler import create_connection, execute_query, close_connection,
 from lookups import DESTINATION_SCHEMA, InputTypes, ErrorHandling
 from logging_handler import show_error_message
 from misc_handler import execute_sql_folder
+import datetime
 
 def create_etl_checkpoint(db_session):
    schema_destination_table = DESTINATION_SCHEMA.DESTINATION_NAME.value
@@ -17,7 +18,7 @@ def create_etl_checkpoint(db_session):
 def return_etl_last_updated_date(db_session):
    schema_destination_table = DESTINATION_SCHEMA.DESTINATION_NAME.value
    does_etl_time_exists = False
-   query = f""""
+   query = f"""
       SELECT elt_last_run_date from {schema_destination_table}.etl_checkpoint ORDER BY etl_last_run_date DESC LIMIT 1
    """
    try:
@@ -30,21 +31,46 @@ def return_etl_last_updated_date(db_session):
    except Exception as e:
       suffix = str(e)
       error_prefix = ErrorHandling.RETURN_LAST_ETL_DATE_ERROR.value
+      show_error_message(error_prefix, suffix)
    finally:
       return return_date, does_etl_time_exists
          
+def insert_or_update_etl_checkpoint(db_session,does_etl_time_exists,  etl_date = None):
+   schema_destination_table = DESTINATION_SCHEMA.DESTINATION_NAME.value
+   if does_etl_time_exists:
+      update_query = f"""
+   UPDATE {schema_destination_table}.etl_checkpoint SET etl_last_run_date = '{etl_date}'
+   """
+   else:
+      update_query = f"""
+   INSERT INTO {schema_destination_table}.etl_checkpoint (etl_last_run_date) VALUES ('{etl_date}')
+   """
 
-
+   try:
+      execute_query(db_session= db_session, query=update_query)
+   except Exception as e:
+      suffix = str(e)
+      error_prefix = ErrorHandling.RETURN_INSERT_OR_UPDATE_LAST_ETL_ERROR.value
+      show_error_message(error_prefix, suffix)
 
 
 
 
 def excute_hook():
    sql_folder_path = "hook_SQL_commands"
-   db_session = create_connection()
-
-   etl_date , does_etl_time_exists = return_etl_last_updated_date(db_session)
-
-   execute_sql_folder(db_session,sql_folder_path)
-
-   close_connection()
+   try:
+      print("executing hook")
+      db_session = create_connection()
+      print("returning last etl updated date")
+      etl_date , does_etl_time_exists = return_etl_last_updated_date(db_session)
+      print("executing hook sql folder")
+      execute_sql_folder(db_session,sql_folder_path)
+      print("inserting etl checkpoint")
+      insert_or_update_etl_checkpoint(db_session, does_etl_time_exists, datetime.now())
+      print("done")
+      close_connection(db_session=db_session)
+   except Exception as e:
+      suffix = str(e)
+      error_prefix = ErrorHandling.EXECUTE_HOOK_ERROR.value
+      show_error_message(error_prefix, suffix)
+      
