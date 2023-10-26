e42feb94a7a5377bca6ede32b443325b5784a4e0
MahmoudElSayed100 <mahmoudelsayed121810@gmail.com>
2023-10-27 01:23:10 +0300

    fixed the imports in main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from prehook import execute_prehook_csv
+from prehook import execute_prehook
 from hook import execute_hook
 from posthook import execute_posthook
 
 def etl_run():
-   df,df2 = execute_prehook_csv()
+   df,df2 = execute_prehook()
    execute_hook(df,df2)
    execute_posthook()
