0774d2b95711627d388beb4e6e5457076cde70ec
MahmoudElSayed100 <mahmoudelsayed121810@gmail.com>
2023-10-26 23:03:13 +0300

    removed unused imports from main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from prehook import execute_prehook_csv, execute_prehook_API
-from hook import execute_hook, execute_hook_API
+from prehook import execute_prehook_csv
+from hook import execute_hook
 from posthook import execute_posthook
-import time
-import schedule
 
 def etl_run():
    df,df2 = execute_prehook_csv()
