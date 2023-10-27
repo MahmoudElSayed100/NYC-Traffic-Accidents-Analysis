9e2c29c454df894e7aa0c8b923c1de76989ef820
MahmoudElSayed100 <mahmoudelsayed121810@gmail.com>
2023-10-27 22:31:40 +0300

    Added logging to `execute_posthook` in `posthook.py`

--- posthook.py
+++ posthook.py
@@ -1,6 +1,8 @@
+import logging
 from database_handler import create_connection, close_connection, execute_query
 from lookups import ErrorHandling, DESTINATION_SCHEMA
 from logging_handler import show_error_message
+import time
 
 def posthook_cleanup(db_session):
    destination_table = DESTINATION_SCHEMA.DESTINATION_NAME.value
@@ -16,6 +18,7 @@ def posthook_cleanup(db_session):
       show_error_message(error_prefix, suffix)
 
 def execute_posthook():
+   start_time = time.time()
    try:
       db_session = create_connection()
       print("executing post hook")
@@ -24,6 +27,7 @@ def execute_posthook():
       print("stg table 2 truncated!")
       close_connection(db_session=db_session)
       print("Success")
+      logging.info(f"Time taken for execute_posthook : { time.time() - start_time}")
    except Exception as e:
       suffix = str(e)
       error_prefix = ErrorHandling.EXECUTE_POSTHOOK_ERROR.value
