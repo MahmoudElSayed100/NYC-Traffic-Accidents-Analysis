fabd464ee2c5ba54c32406f073585e5be1a9f2b4
MahmoudElSayed100 <mahmoudelsayed121810@gmail.com>
2023-10-27 04:22:05 +0300

    added time recordings for execute hook

--- hook.py
+++ hook.py
@@ -4,7 +4,7 @@ from logging_handler import show_error_message
 from misc_handler import execute_sql_folder
 from datetime import datetime
 import pandas as pd
-
+import time
 
 def create_etl_checkpoint(db_session):
    schema_destination_table = DESTINATION_SCHEMA.DESTINATION_NAME.value
@@ -74,54 +74,76 @@ def insert_or_update_etl_checkpoint(db_session,does_etl_time_exists,  etl_date =
       show_error_message(error_prefix, suffix)
 
 
-def execute_hook(df,df2):
-   table_name = "All_Accidents"
-   table_name2 = "all_injuries"
-   sql_folder_path = r"C:\Users\Admin\Desktop\SEF-Final-Project-NYC-Accidents-Analysis\hook_SQL_commands"
-   try:
-      print("executing hook")
-      db_session = create_connection()
-      print("creating etl checkpoint")
-      create_etl_checkpoint(db_session=db_session)
-      print("returning last etl updated date")
+import time
 
-      return_date , does_etl_time_exists = return_etl_last_updated_date(db_session)
-      return_date = pd.to_datetime(return_date)
-      print(return_date)
-      df = filter_df_by_etl_date(df , return_date)
-      return_date
-      
+def execute_hook(df, df2):
+    table_name = "All_Accidents"
+    table_name2 = "all_injuries"
+    sql_folder_path = r"C:\Users\Admin\Desktop\SEF-Final-Project-NYC-Accidents-Analysis\hook_SQL_commands"
+    try:
+        start_time = time.time()  # Record the start time
+        print("executing hook")
+        db_session = create_connection()
+        print(f"Time taken for create_connection: {time.time() - start_time} seconds")
 
-      df2 = filter_df_by_etl_date_API(df2, return_date)
-      
-      print("Creating insert into staging tables from source1")
-      insert_query = return_insert_into_sql_statement_from_df_stg(df,table_name)
-      print("inserting data into stg")
-      for query in insert_query:
-         execute_query(db_session,query)
-      print("Success")
+        start_time = time.time()
+        print("creating etl checkpoint")
+        create_etl_checkpoint(db_session=db_session)
+        print(f"Time taken for create_etl_checkpoint: {time.time() - start_time} seconds")
 
-      print("Creating insert into staging tables from source2")
-      insert_query2 = return_insert_into_sql_statement_from_df_stg(df2,table_name2)
-      print("inserting data into stg")
-      for query in insert_query2:
-         execute_query(db_session,query)
-      print("Success")
+        start_time = time.time()
+        print("returning last etl updated date")
+        return_date, does_etl_time_exists = return_etl_last_updated_date(db_session)
+        return_date = pd.to_datetime(return_date)
+        print(f"Time taken for return_etl_last_updated_date: {time.time() - start_time} seconds")
+        print(return_date)
 
-      print("executing sql folder")
-      execute_sql_folder(db_session,sql_folder_path)
+        start_time = time.time()
+        df = filter_df_by_etl_date(df, return_date)
+        return_date
+        print(f"Time taken for filter_df_by_etl_date: {time.time() - start_time} seconds")
 
-      print("inserting etl checkpoint")
+        start_time = time.time()
+        df2 = filter_df_by_etl_date_API(df2, return_date)
+        print(f"Time taken for filter_df_by_etl_date_API: {time.time() - start_time} seconds")
 
+        start_time = time.time()
+        print("Creating insert into staging tables from source1")
+        insert_query = return_insert_into_sql_statement_from_df_stg(df, table_name)
+        print(f"Time taken for return_insert_into_sql_statement_from_df_stg (source1): {time.time() - start_time} seconds")
+        print("inserting data into stg")
+        for query in insert_query:
+            execute_query(db_session, query)
+        print("Success")
 
-      newest_date = df['CRASH_DATE'].max().date()
-      insert_or_update_etl_checkpoint(db_session, does_etl_time_exists, newest_date)
-      print("done")
-      close_connection(db_session=db_session)
-   except Exception as e:
-      suffix = str(e)
-      error_prefix = ErrorHandling.EXECUTE_HOOK_ERROR.value
-      show_error_message(error_prefix, suffix)
+        start_time = time.time()
+        print("Creating insert into staging tables from source2")
+        insert_query2 = return_insert_into_sql_statement_from_df_stg(df2, table_name2)
+        print(f"Time taken for return_insert_into_sql_statement_from_df_stg (source2): {time.time() - start_time} seconds")
+        print("inserting data into stg")
+        for query in insert_query2:
+            execute_query(db_session, query)
+        print("Success")
+
+        start_time = time.time()
+        print("executing sql folder")
+        execute_sql_folder(db_session, sql_folder_path)
+        print(f"Time taken for execute_sql_folder: {time.time() - start_time} seconds")
+
+        print("inserting etl checkpoint")
+
+        start_time = time.time()
+        newest_date = df['CRASH_DATE'].max().date()
+        insert_or_update_etl_checkpoint(db_session, does_etl_time_exists, newest_date)
+        print(f"Time taken for insert_or_update_etl_checkpoint: {time.time() - start_time} seconds")
+
+        print("done")
+
+        close_connection(db_session=db_session)
+    except Exception as e:
+        suffix = str(e)
+        error_prefix = ErrorHandling.EXECUTE_HOOK_ERROR.value
+        show_error_message(error_prefix, suffix)
 
 
 
