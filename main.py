4131e275a11ee9076fccc07abfa3b1ee3c1fd1ea
MahmoudElSayed100 <mahmoudelsayed121810@gmail.com>
2023-10-27 22:20:08 +0300

    Added logging to `main.py`

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import logging
 from prehook import execute_prehook
 from hook import execute_hook
 from posthook import execute_posthook
+
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
 def etl_run():
    df,df2 = execute_prehook()
